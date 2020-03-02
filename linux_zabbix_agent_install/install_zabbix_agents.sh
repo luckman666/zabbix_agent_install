d56e58e059e0e507af06c789409e2c1c3ec3c051
波哥的IT人生 <33419312+luckman666@users.noreply.github.com>
2020-03-02 13:48:05 +0800

    增加网络问题重试机制

--- linux_zabbix_agent_install/install_zabbix_agents.sh
+++ linux_zabbix_agent_install/install_zabbix_agents.sh
@@ -11,14 +11,30 @@ else
 rpm -ivh  http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
 fi
 
+num=0
+while true ; do
+let num+=1
 yum -y install zabbix-agent
-systemctl enable zabbix-agent
+if [[ $? -eq 0 ]] ; then
 
+echo "zabbix-agent安装环境配置完成！！！"
+systemctl enable zabbix-agent
 cat >> /etc/zabbix/zabbix_agentd.conf << EOF
 Server=$Server
 ServerActive=$ServerActive
 Hostname=$Hostname
 EOF
 systemctl start zabbix-agent
+
+break;
+else
+if [[ num -gt 5 ]];then
+echo "尝试安装zabbix-agent 5次依然无法安装请检查网络问题"
+break
+fi
+echo "FK!~zabbix-agent没成功？哥再来一次！！"
+fi
+done
+
 }
 install_zabbix
